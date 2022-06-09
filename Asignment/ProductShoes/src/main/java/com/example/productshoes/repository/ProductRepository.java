50d44e3cb6fb0ddc02b74fc1eb7b293c250892b1
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2022-06-09 21:18:16 +0700

    .

--- Asignment/ProductShoes/src/main/java/com/example/productshoes/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.productshoes.repository;
-
-import com.example.productshoes.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product,String> {
-
-}
-
